91e98ebd88ea6e0bc6a085e1a61757b3acb815d1
Antonio Sucre <antonio.sucre@indikatore.com>
2021-12-12 15:50:05 -0300

    S1 - hook useState

--- front/pro-atividade-app/src/App.js
+++ front/pro-atividade-app/src/App.js
@@ -1,23 +1,34 @@
+import { useState } from 'react';
 import './App.css';
 
-function App() {
-  const atividades = [
-    { id: 1, descricao: "Primeira atividade" },
-    { id: 2, descricao: "Segunda atividade" }
+let initialState = [
+  { id: 1, descricao: "Primeira atividade" },
+  { id: 2, descricao: "Segunda atividade" }
+];
 
-  ];
+function App() {
+  const [atividades, setAtividades] = useState(initialState);
+
+  const addAtividade = (e) => {
+    e.preventDefault();
+    const atividade = {
+      id: document.getElementById('id').value,
+      descricao: document.getElementById('descricao').value,
+    }
+    setAtividades([...atividades, { ...atividade }]);
+  }
   return (
     <>
       <form>
-        <input id="descricao" type="text" placeholder="id" />
-        <input id="descricao" type="text" placeholder="id" />
-        <button>+ Atividade</button>
+        <input id="id" type="text" placeholder="id" />
+        <input id="descricao" type="text" placeholder="descricao" />
+        <button onClick={addAtividade}>+ Atividade</button>
       </form>
       <div className="mt-3">
         <h1>React .NETCore EfCore</h1>
         <ul className='list-group'>
           {atividades.map(a => (
-            <li key={a.id} className='list-group-item'>{a.descricao}</li>
+            <li key={a.id} className='list-group-item'>{a.id} - {a.descricao}</li>
           ))}
         </ul>
       </div>
