4daacc311bf621fbf5fd5c71f82e2d16dcd33572
Antonio Sucre <antonio.sucre@indikatore.com>
2021-12-12 18:04:47 -0300

    S1 - componente Atividade

--- front/pro-atividade-app/src/App.js
+++ front/pro-atividade-app/src/App.js
@@ -29,31 +29,7 @@ function App() {
     setAtividades([...atividades, { ...atividade }]);
   }
 
-  const prioridadeLabel = (param) => {
-    switch (param) {
-      case '1':
-        return 'Baixa';
-      case '2':
-        return 'Normal';
-      case '3':
-        return 'Alta';
-      default:
-        return 'Não definido';
-    }
-  }
 
-  const prioridadeStyle = (param, icone) => {
-    switch (param) {
-      case '1':
-        return icone ? 'smile' : 'success';
-      case '2':
-        return icone ? 'meh' : 'dark';
-      case '3':
-        return icone ? 'frown' : 'warning';
-      default:
-        return 'Não definido';
-    }
-  }
 
   return (
     <>
@@ -64,36 +40,12 @@ function App() {
       />
       <div className="mt-3">
         {atividades.map(a => (
+          <Atividade
+            key={a.id}
+            a={a}
+            deletarAtividade={deletarAtividade}
+          />
 
-          <div key={a.id} className={'card mb-2 shadow-sm border-' + prioridadeStyle(a.prioridade)} >
-            <div className='card-body'>
-              <div className='d-flex justify-content-between'>
-                <h5 className='card-title'>
-                  <span className='badge bg-secondary me-1'>{a.id}</span>
-                  {a.titulo}
-                </h5>
-                <h6>Prioridade:
-                  <span className={'ms-1 text-' + prioridadeStyle(a.prioridade)}>
-                    <i className={'me-1 far fa-' + prioridadeStyle(a.prioridade, true)}></i>
-                    {prioridadeLabel(a.prioridade)}
-                  </span>
-                </h6>
-              </div>
-              <p className='card-text'>{a.descricao}</p>
-              <div className='d-flex justify-content-end border-top pt-2'>
-                <button className='btn btn-outline-primary me-2 btn-sm'>
-                  <i className="fas fa-pen me-2"></i>
-                  Editar
-                </button>
-                <button className='btn btn-outline-danger me-2 btn-sm'
-                  onClick={() => deletarAtividade(a.id)}>
-                  <i className="fas fa-trash me-2"></i>
-                  Deletar
-                </button>
-
-              </div>
-            </div>
-          </div>
         ))}
       </div>
     </>
--- front/pro-atividade-app/src/components/Atividade.js
+++ front/pro-atividade-app/src/components/Atividade.js
@@ -1,9 +1,61 @@
 import React from "react";
 
 export const Atividade = props => {
-    return (
-        <div>
+    const prioridadeLabel = (param) => {
+        switch (param) {
+            case '1':
+                return 'Baixa';
+            case '2':
+                return 'Normal';
+            case '3':
+                return 'Alta';
+            default:
+                return 'Não definido';
+        }
+    }
 
+    const prioridadeStyle = (param, icone) => {
+        switch (param) {
+            case '1':
+                return icone ? 'smile' : 'success';
+            case '2':
+                return icone ? 'meh' : 'dark';
+            case '3':
+                return icone ? 'frown' : 'warning';
+            default:
+                return 'Não definido';
+        }
+    }
+
+    return (
+        <div className={'card mb-2 shadow-sm border-' + prioridadeStyle(props.a.prioridade)} >
+            <div className='card-body'>
+                <div className='d-flex justify-content-between'>
+                    <h5 className='card-title'>
+                        <span className='badge bg-secondary me-1'>{props.a.id}</span>
+                        {props.a.titulo}
+                    </h5>
+                    <h6>Prioridade:
+                        <span className={'ms-1 text-' + prioridadeStyle(props.a.prioridade)}>
+                            <i className={'me-1 far fa-' + prioridadeStyle(props.a.prioridade, true)}></i>
+                            {prioridadeLabel(props.a.prioridade)}
+                        </span>
+                    </h6>
+                </div>
+                <p className='card-text'>{props.a.descricao}</p>
+                <div className='d-flex justify-content-end border-top pt-2'>
+                    <button className='btn btn-outline-primary me-2 btn-sm'>
+                        <i className="fas fa-pen me-2"></i>
+                        Editar
+                    </button>
+                    <button className='btn btn-outline-danger me-2 btn-sm'
+                        onClick={() => props.deletarAtividade(props.a.id)}>
+                        <i className="fas fa-trash me-2"></i>
+                        Deletar
+                    </button>
+
+                </div>
+            </div>
         </div>
     )
 }
