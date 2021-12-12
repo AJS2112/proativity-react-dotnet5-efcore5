15ee2efd5ee3c3c3a279f137a3117436ccab8314
Antonio Sucre <antonio.sucre@indikatore.com>
2021-12-12 16:14:33 -0300

    S1 - criando card

--- front/pro-atividade-app/src/App.js
+++ front/pro-atividade-app/src/App.js
@@ -19,20 +19,33 @@ function App() {
   }
   return (
     <>
-      <form>
-        <input id="id" type="text" placeholder="id" />
-        <input id="descricao" type="text" placeholder="descricao" />
-        <button onClick={addAtividade}>+ Atividade</button>
+      <h1>React .NETCore EfCore</h1>
+      <form className="row g-3">
+        <div className="col-md-6">
+          <label for="id" className="form-label">Id</label>
+          <input id="id" className="form-control" type="text" placeholder="id" />
+        </div>
+        <div className='col-md-6'>
+          <label for="descricao" className='form-label'>Descricao</label>
+          <input id="descricao" className='form-control' type="text" placeholder="descricao" />
+        </div>
+        <hr />
+        <button className='btn btn-outline-sencondary' onClick={addAtividade}>+ Atividade</button>
+        {/* <div className='col-12'>
+        </div> */}
       </form>
+
       <div className="mt-3">
-        <h1>React .NETCore EfCore</h1>
-        <ul className='list-group'>
-          {atividades.map(a => (
-            <li key={a.id} className='list-group-item'>{a.id} - {a.descricao}</li>
-          ))}
-        </ul>
+        {atividades.map(a => (
+          <div key={a.id} className='card mb-2 shadow-sm' >
+            <div className='card-body'>
+              <p className='card-text'>{a.id} - {a.descricao}</p>
+            </div>
+          </div>
+        ))}
       </div>
     </>
+
   );
 }
 
