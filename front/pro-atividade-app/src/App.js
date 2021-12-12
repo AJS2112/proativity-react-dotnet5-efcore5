59c465aa1c0d58df7487d1bbecd713ce13654f6f
Antonio Sucre <antonio.sucre@indikatore.com>
2021-12-12 15:32:53 -0300

    S1 - atividade map

--- front/pro-atividade-app/src/App.js
+++ front/pro-atividade-app/src/App.js
@@ -1,10 +1,27 @@
 import './App.css';
 
 function App() {
+  const atividades = [
+    { id: 1, descricao: "Primeira atividade" },
+    { id: 2, descricao: "Segunda atividade" }
+
+  ];
   return (
-    <div className="mt-3">
-      <h1>React .NETCore EfCore</h1>
-    </div>
+    <>
+      <form>
+        <input id="descricao" type="text" placeholder="id" />
+        <input id="descricao" type="text" placeholder="id" />
+        <button>+ Atividade</button>
+      </form>
+      <div className="mt-3">
+        <h1>React .NETCore EfCore</h1>
+        <ul className='list-group'>
+          {atividades.map(a => (
+            <li key={a.id} className='list-group-item'>{a.descricao}</li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
 }
 
